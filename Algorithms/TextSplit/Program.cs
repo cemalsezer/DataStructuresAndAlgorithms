22b2b1fcb3d1829e66a8a1a997dc47abdf1f9f63
CemalSezer <infocemalsezer@gmail.com>
2024-06-13 19:35:14 +0300

    Algorithms file is builded and textsplit algorithm is solved

---
+++ Algorithms/TextSplit/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TextSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "hello world this is a test sentence";
+            string[] newText = text.Split(' ');
+
+            for (int i = 0; i < newText.Length; i++) {
+                newText[i] = char.ToUpper(newText[i][0]) + newText[i].Substring(1);
+                
+                if (i < newText.Length - 1)
+                {
+                    Console.Write($"{newText[i]}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{newText[i]}");
+                }
+            }
+        }
+    }
+}
