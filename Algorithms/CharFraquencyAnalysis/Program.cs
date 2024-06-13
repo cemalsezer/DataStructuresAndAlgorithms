9d9f37963148f6cbd6da1ed8dca6a841d265be6c
CemalSezer <infocemalsezer@gmail.com>
2024-06-13 23:14:10 +0300

    CharFraquencyAnalysis algorithm is added

---
+++ Algorithms/CharFraquencyAnalysis/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace CharFraquencyAnalysis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Verilen bir metin içindeki harflerin frekansını bulan bir C# fonksiyonu nasıl yazarsınız?
+             * Fonksiyon, bir metin dizesi alacak ve metindeki her bir harfin kaç kez geçtiğini sayacak şekilde tasarlanmalıdır. 
+             * Fonksiyonunuzun büyük-küçük harf duyarlılığı olup olmadığını belirtmek önemlidir. 
+             */
+            string text = "galatasaray şampiyon oldu"; //m:1 e:1 r:1 h:1 a:2 b:1
+
+            CharFrequencyAnalysis(text);
+        }
+
+        private static void CharFrequencyAnalysis(string input)
+        {
+            input = input.Replace(" ", "");
+            var keyValuePairs = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (keyValuePairs.ContainsKey(c))
+                {
+                    keyValuePairs[c]++;
+                }
+                else
+                {
+                    keyValuePairs.Add(c, 1);
+                }
+            }
+
+            foreach (var c in keyValuePairs)
+            {
+                Console.WriteLine($"{c.Key}:{c.Value}");
+            }
+
+        }
+
+
+    }
+}
