74e1aedd6d0e9b84c2739d19935ce709448996dd
CemalSezer <infocemalsezer@gmail.com>
2024-06-19 20:44:51 +0300

    FibonacciSeries' algorithm added to Algorithms file

---
+++ Algorithms/FibonacciSeries/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace FibonacciSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindFibonacciSeries(10);
+        }
+
+        private static void FindFibonacciSeries(int targetNum)
+        {
+            int firstNum = 0;
+            int secondNum = 1;
+            int totalNum = 0;
+
+            for (int i = 0; i < targetNum; i++)
+            {
+                Console.Write($"{firstNum} ");
+                totalNum = firstNum + secondNum;  
+                firstNum = secondNum;
+                secondNum = totalNum;
+            }
+        }
+    }
+}
