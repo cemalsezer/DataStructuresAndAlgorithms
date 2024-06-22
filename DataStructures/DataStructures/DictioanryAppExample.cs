b0a7006f8d5cb2ca069531ce4afd016448f4d247
CemalSezer <infocemalsezer@gmail.com>
2024-06-22 16:05:49 +0300

    Dictionary Structure is added in DataStructure

---
+++ DataStructures/DataStructures/DictioanryAppExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class DictioanryAppExample
+    {
+        public static void Payment()
+        {
+            var employeeList = new Dictionary<int, Employee>()
+            {
+                {100, new Employee("A","B",1000) },
+                {101, new Employee("C","B",2000) }
+            };
+
+            employeeList.Add(102, new Employee("E", "E", 2000));
+
+            foreach (var item in employeeList)
+            {
+                Console.WriteLine(item);
+            }
+        } 
+    }
+}
