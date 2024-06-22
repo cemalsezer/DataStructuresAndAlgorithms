12abd2cc13be5c3754d435ec36441e45cfa7f91b
CemalSezer <infocemalsezer@gmail.com>
2024-06-22 16:06:37 +0300

    SortedDictionary Structure is added in DataStructures

---
+++ DataStructures/DataStructures/SortedDictionaryAppExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class SortedDictionaryAppExample
+    {
+        public static void BookIndexApp()
+        {
+            var bookIndex = new SortedDictionary<string, List<int>>()
+            {
+                {"HTML", new List<int>() {8,10,12}},
+                {"CSS", new List<int>() {70,80,90}},
+                {"JQuery", new List<int>() {3,5,15}},
+                {"SQL", new List<int>() {70,80}}
+
+            };
+
+            bookIndex.Add("FTP", new List<int>() { 3, 5, 7 });
+            bookIndex.Add("ASP.NET", new List<int>() { 50,60 });
+
+            foreach(var item in bookIndex)
+            {
+                Console.WriteLine(item.Key);
+                item.Value.ForEach(x => Console.WriteLine("\t >"+x));
+            }
+        }
+    }
+}
