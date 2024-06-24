f6eeb089044081ff3bc1bc273c574da4d4ae853b
CemalSezer <infocemalsezer@gmail.com>
2024-06-24 22:31:25 +0300

    PrimeNumber is added in Algorithms

---
+++ Algorithms/PrimeNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool result = IsPrime.IsNumberPrime(5);
+            Console.WriteLine(result == true ? "Num is prime" : "Num is not prime");
+
+        }
+    }
+}
