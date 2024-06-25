10bac88f89f64fac9dfcda9036c67bf4f4d23479
CemalSezer <infocemalsezer@gmail.com>
2024-06-25 21:45:05 +0300

    SumOfNumbers algorithm is added in Algorithms

---
+++ Algorithms/SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace SumOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 525;
+            SumOfNumbers(x);
+        }
+
+        private static void SumOfNumbers(int x)
+        {
+            int sum = 0, num = 0;
+            while(x > 0)
+            {
+                num = x % 10;
+                sum += num;
+                x /= 10;
+            }
+            Console.WriteLine("Sum of numbers "+sum);
+        }
+    }
+}
