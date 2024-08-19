4d385b6ebecaf8f263207a70a56723a47269a2a2
CemalSezer <infocemalsezer@gmail.com>
2024-08-19 15:20:17 +0300

    Expert DataStructures operations file is builded
    Dynamic Array Class builded and adding operation is done

---
+++ DataStructures/Expert/Apps/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Apps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new DataStructures.Array.Array<int>();
+            arr.Add(1);
+            arr.Add(2);
+            arr.Add(3);
+            arr.Add(4);
+            arr.Add(5);
+
+            Console.WriteLine($"{arr.Count} - {arr.Capacity}");
+        }
+    }
+}
---
+++ DataStructures/Expert/DataStructures/Array/Array.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Array
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        private T[] InnerList;
+        public int Count { get; private set; }
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList = new T[2];
+            Count=0;
+        }
+        public void Add(T item)
+        {
+            if(InnerList.Length == Count)
+            {
+                DoubleArray();
+            }
+            InnerList[Count] = item;
+            Count++;
+        }
+
+        private void DoubleArray()
+        {
+            var temp = new T[InnerList.Length*2];
+            System.Array.Copy(InnerList, temp, InnerList.Length);
+            InnerList = temp;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
