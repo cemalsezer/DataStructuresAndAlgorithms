c33b4ff74a30cf71ad34dc9fb7a0c001018d6fdc
CemalSezer <infocemalsezer@gmail.com>
2024-08-20 17:33:49 +0300

    AddFirst operation on Singlety LinkedList

---
+++ DataStructures/Expert/DataStructures/LinkedList/SinglyLinkedList/SingleyLindkedListNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    public class SingleyLindkedListNode<T>
+    {
+        public SingleyLindkedListNode<T> Next { get; set; }
+        public T Value { get; set; }
+
+        public SingleyLindkedListNode(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}";
+        }
+    }
+}
---
+++ DataStructures/Expert/DataStructures/LinkedList/SinglyLinkedList/SinglyLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.LinkedList.SinglyLinkedList
+{
+    public class SinglyLinkedList<T>
+    {
+        public SingleyLindkedListNode<T> Head{ get; set; }
+
+        public void AddFirst(T value)
+        {
+            var newNode= new SingleyLindkedListNode<T>(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        public void AddLast(T value)
+        {
+            var newNode = new SingleyLindkedListNode<T>(value);
+            if(Head == null)
+            {
+                Head= newNode;
+                return;
+            }
+
+            var current = Head;
+            while(current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+}
