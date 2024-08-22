af4242882ab6d58637b55e30e61462d4d8ecbee4
CemalSezer <infocemalsezer@gmail.com>
2024-08-22 15:55:02 +0300

    Stack app is done

--- DataStructures/Expert/Apps/Program.cs
+++ DataStructures/Expert/Apps/Program.cs
@@ -11,6 +11,31 @@ namespace Apps
 
         }
 
+        private static void StackApp()
+        {
+            var charset = new char[] { 'a', 'b', 'c', 'd', 'e' };
+            var stack1 = new DataStructures.Stack.Stack<char>();
+            var stack2 = new DataStructures.Stack.Stack<char>();
+            foreach (char c in charset)
+            {
+                Console.WriteLine(c);
+                stack1.Push(c);
+                stack2.Push(c);
+            }
+            Console.WriteLine("Peek");
+            Console.WriteLine($"Stack1:{stack1.Peek()}");
+            Console.WriteLine($"Stack2:{stack2.Peek()}");
+
+            Console.WriteLine("\nCount");
+            Console.WriteLine($"Stack1:{stack1.Count}");
+            Console.WriteLine($"Stack2:{stack2.Count}");
+
+
+            Console.WriteLine("\nPop");
+            Console.WriteLine($"Stack1:{stack1.Pop()} has been removed");
+            Console.WriteLine($"Stack2:{stack2.Pop()} has been removed");
+        }
+
         private static void DbLinkedListApp()
         {
             var list = new DoublyLinkedList<int>();
