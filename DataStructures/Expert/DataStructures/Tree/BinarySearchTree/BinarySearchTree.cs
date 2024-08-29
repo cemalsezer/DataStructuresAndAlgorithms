9a74232983cd0ed45e47599cf7c617e6c6088555
CemalSezer <infocemalsezer@gmail.com>
2024-08-29 16:20:14 +0300

    Binary Search Tree Class design is done

---
+++ DataStructures/Expert/DataStructures/Tree/BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree.BinarySearchTree
+{
+    public class BinarySearchTree<T> : IEnumerable<T>
+        where T: IComparable
+    {
+        public Node<T> Root { get; set; }
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        public BinarySearchTree()
+        {
+            
+        }
+    }
+}
