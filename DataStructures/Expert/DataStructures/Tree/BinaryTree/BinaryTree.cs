f095705763b6147b47de24e791a0efe014d9873e
CemalSezer <infocemalsezer@gmail.com>
2024-09-03 19:35:49 +0300

    InOrderNonRecursiveTraversal Prop is added

--- DataStructures/Expert/DataStructures/Tree/BinaryTree/BinaryTree.cs
+++ DataStructures/Expert/DataStructures/Tree/BinaryTree/BinaryTree.cs
@@ -23,6 +23,35 @@ namespace DataStructures.Tree.BinaryTree
             }
             return list;
         }
+        public List<Node<T>> InOrderNonRecursiveTraversal(Node<T> root)
+        {
+            var list = new List<Node<T>>();
+            var S = new DataStructures.Stack.Stack<Node<T>>();
+            Node<T> currentNode = root;
+            bool done = false;
+            while(!done)
+            {
+                if (currentNode != null)
+                {
+                    S.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+                else
+                {
+                    if(S.Count==0)
+                    {
+                        done = true;
+                    }
+                    else
+                    {
+                        currentNode = S.Pop();
+                        list.Add(currentNode);
+                        currentNode = currentNode.Right;
+                    }
+                }
+            }
+            return list;
+        }
         public List<Node<T>> PreOrder(Node<T> root)
         {
             if (!(root == null))
