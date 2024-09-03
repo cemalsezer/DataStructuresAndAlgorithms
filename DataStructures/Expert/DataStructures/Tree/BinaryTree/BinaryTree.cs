13369d9ab7e4d29dd3264cf7c9b5ce050b74e0a8
CemalSezer <infocemalsezer@gmail.com>
2024-09-03 16:20:42 +0300

    BinaryTree is builded

---
+++ DataStructures/Expert/DataStructures/Tree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Tree.BinaryTree
+{
+    public class BinaryTree<T> where T:IComparable
+    {
+        public List<Node<T>> list {  get; private set; }
+        public BinaryTree()
+        {
+            list = new List<Node<T>>();
+        }
+        public List<Node<T>> InOrder(Node<T> root)
+        {
+            if(!(root == null))
+            {
+                InOrder(root.Left);
+                list.Add(root);
+                InOrder(root.Right);    
+            }
+            return list;
+        }
+    }
+}
